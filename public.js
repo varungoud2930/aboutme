6e674538f5ed74808f1c5aa432287ec5271e1169
Varun Goud Pulipalpula <varunwaroon@gmail.com>
2019-02-23 20:58:57 -0600

    commit

--- public.js
+++ public.js
@@ -2,3 +2,16 @@ function temperatureConverter(valNum) {
     valNum = parseFloat(valNum);
     document.getElementById("outputcelsius").innerHTML = (valNum - 32) / 1.8;
 }
+
+
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("demo").innerHTML =
+                this.responseText;
+        }
+    };
+    xhttp.open("GET", "bunty.txt", true);
+    xhttp.send();
+}
